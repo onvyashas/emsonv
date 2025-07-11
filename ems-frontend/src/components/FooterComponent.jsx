9eef72ef96e9370fbf0ea877fefd8a02e7a05131
onvyashas <154075790+onvyashas@users.noreply.github.com>
2025-07-12 05:09:10 +0530

    Update FooterComponent.jsx

--- ems-frontend/src/components/FooterComponent.jsx
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -3,10 +3,9 @@ import React from 'react'
 const FooterComponent = () => {
   return (
     <div>
-        <footer className='footer'>
-            <span>All rights reserved 2025 
-        </footer>
-
+      <footer className='footer'>
+        <span>All rights reserved 2025</span>
+      </footer>
     </div>
   )
 }
